489bcf254b98a47baafdc78587c492df1a4b4c69
Adil Sahab <adil.sahab16@gmail.com>
2025-04-12 17:37:02 -0400

    Update run_notebook.py

--- run_notebook.py
+++ run_notebook.py
@@ -1,6 +1,7 @@
 import papermill as pm
-
+import sys
 pm.execute_notebook(
     'hello_world.ipynb',
     'hello_world_out.ipynb'  # output log of execution
 )
+sys.exit(0)  # Graceful exit to indicate success
